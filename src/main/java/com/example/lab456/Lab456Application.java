31c4ea74e6fcb8b8aa17f5b9614f122c4bc1fcac
Nazariy <nazar260804@gmail.com>
2023-12-11 22:59:48 +0200

    Initial setup

---
+++ src/main/java/com/example/lab456/Lab456Application.java
@@ -0,0 +1,13 @@
+package com.example.lab456;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab456Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab456Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab456/Lab456ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab456;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab456ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
