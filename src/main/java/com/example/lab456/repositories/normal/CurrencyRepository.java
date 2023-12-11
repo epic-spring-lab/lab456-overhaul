3d6749615e6e792904d8f8b78f3b999e000305fb
Nazariy <nazar260804@gmail.com>
2023-12-11 23:37:29 +0200

    Added normal repos

---
+++ src/main/java/com/example/lab456/repositories/normal/CurrencyRepository.java
@@ -0,0 +1,8 @@
+package com.example.lab456.repositories.normal;
+
+import com.example.lab456.entities.CurrencyEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CurrencyRepository extends CrudRepository<CurrencyEntity, Long> {
+
+}
---
+++ src/main/java/com/example/lab456/repositories/normal/ExchangeRateRepository.java
@@ -0,0 +1,8 @@
+package com.example.lab456.repositories.normal;
+
+import com.example.lab456.entities.ExchangeRateEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ExchangeRateRepository extends CrudRepository<ExchangeRateEntity, Long> {
+
+}
